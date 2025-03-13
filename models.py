bcda7e10a3b7f0a2bd613253565428cbea232ca0
Mehran Mirzaei <mehranmirzaeiv@gmail.com>
2025-03-13 23:13:26 +0330

    add database and Todo model to the Project

---
+++ models.py
@@ -0,0 +1,15 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+
+
+class Todo(Base):
+    __tablename__ = "todo"
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    updated_at = Column(DateTime(timezone=True))
+    title = Column(String)
+    todo_body = Column(String)
+    priority = Column(Integer)
+    is_in_progress = Column(Boolean, default=False)
+    is_completed = Column(Boolean, default=False)
+    complete_time = Column(DateTime(timezone=True))
