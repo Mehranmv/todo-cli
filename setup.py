87d76172e400d26fd6d1edcc41034e7c93733616
Mehran Mirzaei <mehranmirzaeiv@gmail.com>
2025-03-17 16:33:25 +0330

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="rich-todo",
-    version="0.1.0",
+    version="0.1.1",
     packages=["todo_cli"],
     package_dir={"todo_cli": "todo_cli"},
     include_package_data=True,
